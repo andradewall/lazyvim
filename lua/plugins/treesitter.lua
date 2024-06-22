1d92f4d34badedebb18d106c09111a74206f5cde
Wallace A <andradewall0@gmail.com>
2024-06-22 15:28:31 -0300

    send files

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,30 @@
+-- Customize Treesitter
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = function(_, opts)
+    opts.ensure_installed = {
+      "lua",
+      "vim",
+      "css",
+      "html",
+      "javascript",
+      "json",
+      "php",
+      "phpdoc",
+      "scss",
+      "php_only",
+      "blade",
+      -- add more arguments for adding more treesitter parsers
+    }
+
+    opts.auto_install = true
+
+    opts.highlight = {
+      enable = true,
+    }
+
+    opts.indent = {
+      enable = true,
+    }
+  end,
+}
