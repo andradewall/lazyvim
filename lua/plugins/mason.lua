ffa1a273560f22b4d1548d88991faf9008d5795b
Wallace A <andradewall0@gmail.com>
2024-07-16 22:22:54 -0300

    feat: add mason

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,11 @@
+return {
+  "williamboman/mason.nvim",
+  cmd = "Mason",
+  opts_extend = { "ensure_installed" },
+  opts = {
+    ensure_installed = {
+      "stylua",
+      "shfmt",
+    },
+  },
+}
