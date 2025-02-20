d663ffcfecace08f0fa606af391b16758144f495
Wallace <andradewall0@gmail.com>
2025-02-20 11:53:12 -0300

    send files

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,79 +1,86 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  event = "VeryLazy",
-  init = function()
-    vim.g.lualine_laststatus = vim.o.laststatus
-    if vim.fn.argc(-1) > 0 then
-      -- set an empty statusline till lualine loads
-      vim.o.statusline = " "
-    else
-      -- hide the statusline on the starter page
-      vim.o.laststatus = 0
-    end
-  end,
-  opts = function()
-    -- PERF: we don't need this lualine require madness 🤷
-    local lualine_require = require("lualine_require")
-    lualine_require.require = require
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    init = function()
+        vim.g.lualine_laststatus = vim.o.laststatus
+        if vim.fn.argc(-1) > 0 then
+            -- set an empty statusline till lualine loads
+            vim.o.statusline = " "
+        else
+            -- hide the statusline on the starter page
+            vim.o.laststatus = 0
+        end
+    end,
+    opts = function()
+        -- PERF: we don't need this lualine require madness 🤷
+        local lualine_require = require("lualine_require")
+        lualine_require.require = require
 
-    vim.o.laststatus = vim.g.lualine_laststatus
+        vim.o.laststatus = vim.g.lualine_laststatus
 
-    local opts = {
-      options = {
-        theme = "nightfly",
-        globalstatus = vim.o.laststatus == 3,
-        disabled_filetypes = { statusline = { "dashboard", "alpha", "ministarter", "snacks_dashboard" } },
-        section_separators = "",
-        component_separators = "",
-      },
-      sections = {
-        lualine_a = { "mode" },
-        lualine_b = {
-          { "branch", icons_enabled = false },
-          { "filetype", icons_enabled = false },
-        },
-        lualine_x = {
-          -- stylua: ignore
-          {
-            function()
-              return require("noice").api.status.command.get()
-            end,
-            cond = function()
-              return package.loaded["noice"] and require("noice").api.status.command.has()
-            end,
-            color = function()
-              return { fg = Snacks.util.color("Statement") }
-            end,
-          },
-          -- stylua: ignore
-          {
-            function() return require("noice").api.status.mode.get() end,
-            cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
-            color = function() return { fg = Snacks.util.color("Constant") } end,
-          },
-          -- stylua: ignore
-          {
-            function() return "  " .. require("dap").status() end,
-            cond = function() return package.loaded["dap"] and require("dap").status() ~= "" end,
-            color = function() return { fg = Snacks.util.color("Debug") } end,
-          },
-          -- stylua: ignore
-          {
-            require("lazy.status").updates,
-            cond = require("lazy.status").has_updates,
-            color = function() return { fg = Snacks.util.color("Special") } end,
-          },
-          {
-            "diff",
-          },
-        },
-        lualine_z = {
-          { "progress", separator = " ", padding = { left = 1, right = 0 } },
-          { "location", padding = { left = 0, right = 1 } },
-        },
-      },
-      extensions = { "neo-tree", "lazy", "fzf" },
-    }
-    return opts
-  end,
+        local opts = {
+            options = {
+                theme = "nightfly",
+                globalstatus = vim.o.laststatus == 3,
+                disabled_filetypes = { statusline = { "dashboard", "alpha", "ministarter", "snacks_dashboard" } },
+                section_separators = "",
+                component_separators = "",
+            },
+            sections = {
+                lualine_a = { "mode" },
+                lualine_b = {
+                    { "branch",   icons_enabled = false },
+                    { "filetype", icons_enabled = false },
+                },
+                lualine_x = {
+                    -- stylua: ignore
+                    {
+                        function()
+                            return require("noice").api.status.command.get()
+                        end,
+                        cond = function()
+                            return package.loaded["noice"] and require("noice").api.status.command.has()
+                        end,
+                        color = function()
+                            return { fg = Snacks.util.color("Statement") }
+                        end,
+                    },
+                    -- stylua: ignore
+                    {
+                        function() return require("noice").api.status.mode.get() end,
+                        cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
+                        color = function() return { fg = Snacks.util.color("Constant") } end,
+                    },
+                    -- stylua: ignore
+                    {
+                        function() return "  " .. require("dap").status() end,
+                        cond = function() return package.loaded["dap"] and require("dap").status() ~= "" end,
+                        color = function() return { fg = Snacks.util.color("Debug") } end,
+                    },
+                    -- stylua: ignore
+                    {
+                        require("lazy.status").updates,
+                        cond = require("lazy.status").has_updates,
+                        color = function() return { fg = Snacks.util.color("Special") } end,
+                    },
+                    {
+                        "diff",
+                    },
+                    {
+                        "location",
+                        -- padding = { left = 0, right = 1 }
+                    },
+                    -- {
+                    --     "progress",
+                    --     separator = " ",
+                    --     -- padding = { left = 1, right = 0 }
+                    -- },
+                },
+                lualine_z = {
+                },
+            },
+            extensions = { "neo-tree", "lazy", "fzf" },
+        }
+        return opts
+    end,
 }
